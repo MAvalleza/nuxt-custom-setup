43ea4306674ced71999dcfb98b77020c05682237
Marc Almond Valleza <majvalleza@gmail.com>
2020-05-04 14:41:12 +0800

    refactor: customize vuetify color defaults

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,17 +54,26 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
       themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
+        // NOTE: These colors are MYCURE defaults
+        light: {
+          primary: '#2e9fdf',
+          accent: '#7fad33',
+          secondary: '#616161',
           info: colors.teal.lighten1,
           warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
+          error: '#ff5252',
+          success: colors.green.accent3,
+        },
+        dark: {
+          primary: '#2e9fdf',
+          accent: '#7fad33',
+          secondary: '#616161',
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: '#ff5252',
+          success: colors.green.accent3,
+        },
       }
     }
   },
