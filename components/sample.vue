0cf1af993ac6ff7de7f63c813cab4d9b3a7bfc4c
Marc Almond Valleza <majvalleza@gmail.com>
2020-05-04 14:42:29 +0800

    refactor: update sample file

--- components/sample.vue
+++ components/sample.vue
@@ -1,14 +1,23 @@
 <template lang="pug">
   div
     | This is a fresh setup
+    pre const a = { b: { c: 'Optional Chaining is enabled!' } }
+    pre a?.b?.c
     p {{chainingEnabled}}
+    div
+      h3 Default Light Color Schemes
+      h4 Customize these at the nuxt.config.js file
+      v-btn(color="primary") Primary
+      v-btn(color="accent") Accent
+      v-btn(color="secondary") Secondary
+      v-btn(color="error") Error
 </template>
 
 <script>
 export default {
   computed: {
     chainingEnabled () {
-      const a = { b: { c: 'Chaining is enabled!' } };
+      const a = { b: { c: 'Optional Chaining is enabled!' } };
       return a?.b?.c;
     },
   },
