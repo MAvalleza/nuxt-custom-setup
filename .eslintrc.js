51707d59307da14e90b77aa7778cb9fd7902d4a3
Marc Almond Valleza <majvalleza@gmail.com>
2020-05-04 13:52:03 +0800

    chore: setup eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,13 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
+    'nuxt/no-cjs-in-config': 'off',
+    'semi': [2, 'always'],
+    'space-before-function-paren': [2, 'always'],
+    'keyword-spacing': [2, { before: true, after: true }],
+    'space-before-blocks': [2, 'always'],
+    'comma-dangle': [2, 'always-multiline'],
+    'no-console': 'off',
+    'no-multi-str': 'off'
   }
 }
